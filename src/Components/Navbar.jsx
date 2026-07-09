b9808ba6f81679434b1a9b17fb8b17a8466469b0
Kanishk-Heera <kanishkheera@gmail.com>
2026-07-09 22:02:29 +0530

    chnaged to red

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -58,12 +58,8 @@ export default function Navbar() {
                 <Drawer.Content
                   css={{
                     "&[data-state='open']": {
-                      animation:
-                        "slide-from-left-full 400ms cubic-bezier(0.32, 0.72, 0, 1) !important",
-                    },
-                    "&[data-state='closed']": {
-                      animation:
-                        "slide-to-left-full 350ms cubic-bezier(0.32, 0.72, 0, 1) !important",
+                      animation: "none",
+                      background: "red",
                     },
                   }}
                 >
