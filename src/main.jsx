1eb22459457c65a35b25fc70758a71e17d7199bf
Kanishk-Heera <kanishkheera@gmail.com>
2026-07-09 23:30:53 +0530

    floating animation added

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "./Components/ui/provider.jsx";
+import './Components/styles/index.css'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
